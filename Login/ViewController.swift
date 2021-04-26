385a529b08bfd3a4195eaf7f8fe27fa6eaab3017
tanvirgeekios <tanvir.alam@iosventure.com>
2021-04-26 09:40:35 +0600

    FBLogin working

--- Login/ViewController.swift
+++ Login/ViewController.swift
@@ -5,15 +5,46 @@
 //  Created by MD Tanvir Alam on 26/4/21.
 //
 
-import UIKit
 
-class ViewController: UIViewController {
 
+
+import FBSDKLoginKit
+
+// Add this to the body
+class ViewController: UIViewController, LoginButtonDelegate {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        if let token = AccessToken.current,!token.isExpired {
+            let token = token.tokenString
+            let request = FBSDKLoginKit.GraphRequest(graphPath: "me", parameters: ["fields":"email, name"], tokenString: token, version: nil, httpMethod: .get)
+            request.start { connection, result, error in
+                print(result)
+            }
+        }else{
+            let loginButton = FBLoginButton()
+            loginButton.center = view.center
+            loginButton.permissions = ["public_profile", "email"]
+            view.addSubview(loginButton)
+            loginButton.delegate = self
+        }
     }
-
-
+    
+    //MARK:- FBLoginDelegate
+    
+    func loginButton(_ loginButton: FBLoginButton, didCompleteWith result: LoginManagerLoginResult?, error: Error?) {
+        let token = result?.token?.tokenString
+        let request = FBSDKLoginKit.GraphRequest(graphPath: "me", parameters: ["fields":"email, name"], tokenString: token, version: nil, httpMethod: .get)
+        request.start { connection, result, error in
+            print(result)
+        }
+    }
+    
+    func loginButtonDidLogOut(_ loginButton: FBLoginButton) {
+        
+    }
 }
+
+
 
